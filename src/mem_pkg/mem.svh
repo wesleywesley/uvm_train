b787a3c7dfd750016d845aa4e5e15a43bfec9af7
Wesley Sheng <wesley.sheng@amd.com>
2020-12-23 17:52:26 +0800

    Fix a typo
    two memory_data, but should data and cmd

--- src/mem_pkg/mem.svh
+++ src/mem_pkg/mem.svh
@@ -10,7 +10,7 @@ class mem extends uvm_sequence_item;
     `uvm_object_utils_begin(mem)
         `uvm_field_int(memory_addr, UVM_ALL_ON)
         `uvm_field_int(memory_data, UVM_ALL_ON)
-        `uvm_field_int(memory_data, UVM_ALL_ON)
+        `uvm_field_int(memory_cmd, UVM_ALL_ON)
     `uvm_object_utils_end
 
     function new(string name = "mem");
